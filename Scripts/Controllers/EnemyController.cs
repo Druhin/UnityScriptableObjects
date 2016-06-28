9785bf33a6adc143f56b609592a0608de1682419
Druhin Mukherjee <druhin.decipher@gmail.com>
2016-06-28 22:02:22 +0530

    Basic structure  to create a new scrip table type

---
+++ Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+	private Text text;
+	private Vector3 destination;
+    public EnemyTemplate enemyTemplate;
+
+	void Awake()
+	{
+		SetRandomDestination();
+		text = GetComponentInChildren<Text>();
+	}
+	
+	void Start()
+	{
+	}
+	
+	void Update()
+	{
+		if( Vector3.Distance( transform.position, destination ) < 0.01f )
+		{
+			SetRandomDestination();
+		}
+        //early exit
+        if (!enemyTemplate)
+            return;
+        if(text)
+        {
+            text.text = "HP\n" + enemyTemplate.maximumHitPoints.ToString();
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, destination, enemyTemplate.speed * Time.deltaTime);
+	}
+	
+	private void SetRandomDestination()
+	{
+		destination = Vector3.left * Random.Range( -4.0f, 4.0f ) + Vector3.forward * Random.Range( -4.0f, 4.0f );
+	}
+}
+
+
+
+
+
+
+
+
---
+++ Scripts/Data/EnemyTemplate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyTemplate : ScriptableObject
+{
+    [Header("Enemy Data")]
+    public int maximumHitPoints;
+    public float speed;
+
+
+
+}
